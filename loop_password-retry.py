f5f0f0662df16c8010c7bd4c3883a1b8ef0a98ee
Patti <pattishhuang@gmail.com>
2024-03-12 18:26:18 +0800

    change to while > 0

--- loop_password-retry.py
+++ loop_password-retry.py
@@ -1,13 +1,15 @@
 # 密碼重試程式 while loop練習
 password = 'a123456'
 x = 3
-while True:
+while x > 0:
+    x = x - 1
     require = input('請輸入密碼')
     if require == password:
-    	print('登入成功')
-    	break
-    else:
-        x = x - 1	
-        print('密碼錯誤！ 還有', x ,'次機會')
-        if  x == 0:
-    	    break
+        print('登入成功')
+        break
+    else:   
+        print('密碼錯誤！')
+        if x > 0:
+            print('還有', x ,'次機會')
+        else:
+            print('帳號已封鎖')
